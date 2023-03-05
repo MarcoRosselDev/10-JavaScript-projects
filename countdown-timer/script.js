f3085b65dbb531b442748b37492f5347b97e8844
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-05 05:45:46 -0300

    countdown | days, hours, minutes and second successful

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,13 +1,23 @@
-const newYears = '1 Jan 2023'
+const newYears = '1 Jan 2024'
 
 function countDown() {
     const newYearsDate = new Date(newYears)
     const currentDate = new Date()
 
-    console.log(newYearsDate - currentDate);
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds  / 3600 % 24);
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days, hours, minutes, seconds);
 }
 
 countDown()
 
+setInterval(countDown, 1000)
+
 // output = -5463241193
 // whe nedd convert this on hours, menets and seconds
+
